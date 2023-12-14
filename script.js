a5916fe2a5e24bf6166f97ff855a1e815114ca73
Valentin <93677277+Vali644@users.noreply.github.com>
2023-12-14 23:44:37 +0200

    Add drawing functionality

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const createGrid = (size) => {
     const container = document.querySelector("#container");
     let row;
-    let segment;
     for (let i = 0; i < size; i++) {
         row = document.createElement('div');
         row.style.cssText = "flex-grow: 1; display: flex; align-content: stretch;"
         for (let i = 0; i < size; i++) {
-            segment = document.createElement('div');
+            let segment = document.createElement('div');
             segment.style.flexGrow = '1';
+            let s = segment;
+            segment.addEventListener("mouseover", () => {
+                segment.style.backgroundColor = "black";
+            })
             row.appendChild(segment);
         }
         container.appendChild(row);
     }
 }
-createGrid(16);
+createGrid(2);
