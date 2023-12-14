026734e71ccb865c2c9ad23259f16b9e37dfec52
Valentin <93677277+Vali644@users.noreply.github.com>
2023-12-14 23:28:09 +0200

    Add grid creation function

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const addSegments = (size) => {
-    for (let i = 0; i < size * size; i++) {
-        console.log(i);
+const createGrid = (size) => {
+    const container = document.querySelector("#container");
+    let row;
+    let segment;
+    for (let i = 0; i < size; i++) {
+        row = document.createElement('div');
+        row.style.cssText = "flex-grow: 1; display: flex; align-content: stretch;"
+        for (let i = 0; i < size; i++) {
+            segment = document.createElement('div');
+            segment.style.flexGrow = '1';
+            row.appendChild(segment);
+        }
+        container.appendChild(row);
     }
 }
-addSegments(2);
+createGrid(16);
